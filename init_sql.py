8af16d89e5cbd7800e969c470b1d2e66180d22c1
Jessica <jwils117@emich.edu>
2017-11-07 15:59:04 -0500

    create table in database

---
+++ init_sql.py
@@ -0,0 +1,38 @@
+import sqlalchemy
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+
+import yaml
+
+CONFIG_FILE_NAME = "config.yaml"
+
+with open(CONFIG_FILE_NAME, 'r') as f:
+    CONFIG = yaml.load(f)['postgres']
+
+def connect(user, password, db, host='localhost', port=5432):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of PostgreSQL URL 
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    # The return value of create_engine is our connection
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    return con, meta
+
+# Connect to DB to initialize
+con, meta = connect(CONFIG['user'], CONFIG['password'], CONFIG['db'])
+
+# Below are all the tables that need to be created
+results = Table('results', meta,
+                Column('date', sqlalchemy.types.DateTime),
+                Column('file_name', String),
+                Column('file_path', String),
+                Column('plugin', String),
+                Column('result', sqlalchemy.types.JSON))
+
+
+# Create the above table(s)
+meta.create_all(con)

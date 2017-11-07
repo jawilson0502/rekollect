498d6a4cc52ebeabdd16cf15e00a27420ac70ee4
Jessica <jwils117@emich.edu>
2017-11-07 15:58:37 -0500

    start building flask api

---
+++ views.py
@@ -0,0 +1,20 @@
+from rekollect import app
+
+@app.route('/')
+def index():
+    return "Welcome to Rekollect!"
+
+@app.route('/api/<file_name>')
+def check_file_name(file_name):
+    '''Returns all data for file_name if file_name already exists'''
+    pass
+
+@app.route('/api/<file_name>/<plugin>', methods=['GET', 'POST'])
+def use_plugin(file_name, plugin):
+    '''For GET request, return JSON for results
+       For POST requests, run that plugin
+    '''
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass

a7bd3fdfcba519a8589c2a829ae82546142e6eae
Jessica <jwils117@emich.edu>
2017-11-19 16:20:12 -0500

    connect db to app

--- web_rekollect/__init__.py
+++ web_rekollect/__init__.py
@@ -1,4 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+
 app = Flask(__name__)
 
 import web_rekollect.views
+import yaml
+
+CONFIG_FILE_NAME = 'config.yaml'
+with open(CONFIG_FILE_NAME, 'r') as f:
+    DB_CONFIG = yaml.load(f)['postgres']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONFIG['uri']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
